12d309af93e75b26d2a12f56d39a8b2c2c49e956
Christopher L. Stevenson <c.stevenson@ieee.org>
2012-12-10 23:42:06 -0500

    simulation is working and added parameters for protocol and port processing

--- dns-regex/dns_regex.vhd
+++ dns-regex/dns_regex.vhd
@@ -31,7 +31,9 @@ use IEEE.STD_LOGIC_UNSIGNED.ALL;
 --use UNISIM.VComponents.all;
 
 entity dns_regex is
-    Port ( clk 				: in  STD_LOGIC;
+	generic (PROTOCOL: STD_LOGIC_VECTOR (7 downto 0) := X"11"; -- UDP
+	         PORT_NUM: STD_LOGIC_VECTOR (15 downto 0) := X"0035");
+   Port (  clk 				: in  STD_LOGIC;
            reset 				: in  STD_LOGIC;
            data_in 			: in  STD_LOGIC_VECTOR (7 downto 0);
            sof_in_n 			: in  STD_LOGIC;
@@ -44,21 +46,20 @@ end dns_regex;
 architecture Behavioral of dns_regex is
 
 type state_type is 
-	(st_idle, st_any_char_counter, st_proto, st_port1, st_port2, st_match, st_no_match);
+	(st_idle, st_any_char_counter, st_proto, st_any_char_counter2, st_port1, st_port2, st_match, st_no_match);
 signal state_reg, state_next: state_type;
 signal match_buf_reg, match_next, rdy_buf_reg, rdy_next: std_logic;
 
 -- counter signals
-signal offset : std_logic_vector(10 downto 0) := (others=>'0');
+signal offset : std_logic_vector(7 downto 0) := (others=>'0');
 signal clear : std_logic := '0';
-signal enable : std_logic := '1';
 
 begin
 
-	-- instantiate 8-bit counter
-	counter_11_unit : entity work.counter(behv)
-		generic map(N=>11)
-		port map(clk=>clk, clear=>clear, enable=>enable, Q=>offset);
+	-- instantiate 8-bit counter 
+	counter_8_unit : entity work.counter(behv)
+		generic map(N=>8)
+		port map(clk=>clk, clear=>clear, enable=>'1', Q=>offset);
 		
 	-- state register
 	process(clk, reset)
@@ -82,9 +83,14 @@ begin
 		end if;
 	end process;
 	
+	-------------------------------------------------------
+	--                                                   --
+	--    dns filter regex: ^.{23}11.{10,14}0035         --
+	--                                                   --
+	--------------------------------------------------------
+	
 	-- next-state logic
 	process(state_reg, sof_in_n, src_rdy_in_n, data_in, offset)
-	--process(state_reg, sof_in_n, src_rdy_in_n)
 	begin
 		case state_reg is
 			when st_idle =>
@@ -97,20 +103,29 @@ begin
 				end if;
 			when st_any_char_counter => -- count any character
 				clear <= '0';
-				if offset < X"15" then -- 22 (23 incl 0)
+				if offset < X"15" then -- offset < 22
 					state_next <= st_any_char_counter;
 				else
 					state_next <= st_proto;
 				end if;
-			when st_proto => -- check port
-				if data_in = X"11" then -- UDP packet
-					state_next <= st_port1;
+			when st_proto => -- protocol - offset = 23
+				if data_in = PROTOCOL then
+					state_next <= st_any_char_counter2;
+					clear <= '1';
 				else
+					clear <= '0';
 					state_next <= st_no_match;
 				end if;
-			when st_port1 =>
-				if offset < X"24" then -- offset < 36
-					if data_in = X"00" then
+			when st_any_char_counter2 => -- count any character offset < 10
+				clear <= '0';
+				if offset < X"09" then -- < 10 (0-9 = 10 places)
+					state_next <= st_any_char_counter2;
+				else
+					state_next <= st_port1;
+				end if;
+			when st_port1 =>				
+				if offset < X"0E" then -- 14 where the port is located
+					if data_in = PORT_NUM(15 downto 8) then
 						state_next <= st_port2;
 					else
 						state_next <= st_port1;
@@ -119,8 +134,8 @@ begin
 					state_next <= st_no_match;
 				end if;
 			when st_port2 =>
-				if offset <= X"24" then -- offset <= 36
-					if data_in = X"35" then
+				if offset <= X"0E" then -- offset <= 14
+					if data_in = PORT_NUM(7 downto 0) then
 						state_next <= st_match;
 					else
 						state_next <= st_port1;
@@ -143,12 +158,8 @@ begin
 		case state_next is
 			when st_idle =>
 			when st_any_char_counter =>
---				if state_reg = st_idle then
---					clear <= '1';
---				else
---					clear <= '0';
---				end if;
 			when st_proto =>
+			when st_any_char_counter2 =>
 			when st_port1 =>
 			when st_port2 =>
 			when st_match =>
--- dns-regex/isim/dns_regex_tb_isim_beh.exe.sim/work/dns_regex_tb_isim_beh.exe_main.c
+++ dns-regex/isim/dns_regex_tb_isim_beh.exe.sim/work/dns_regex_tb_isim_beh.exe_main.c
@@ -33,8 +33,8 @@ int main(int argc, char **argv)
     ieee_p_3564397177_init();
     ieee_p_3499444699_init();
     ieee_p_3620187407_init();
-    work_a_3243737192_1142133938_init();
-    work_a_1917867827_3212880686_init();
+    work_a_0010332520_1142133938_init();
+    work_a_1166193096_3212880686_init();
     work_a_3459610876_2372691052_init();
 
 
